336a36f39a8d8f7d1f08447bf036795fd6496a46
leehuhlee <burgund32@gmail.com>
2020-06-23 15:36:40 +0900

    StreetFoodFighter Season1

--- StreetFoodFighter1/blog/urls.py
+++ StreetFoodFighter1/blog/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    path('post/china_chengdu/', views.post_edit, name='china_chengdu'),
+    path('post/china_chengdu/', views.post_list, name='china_chengdu'),
 ]
